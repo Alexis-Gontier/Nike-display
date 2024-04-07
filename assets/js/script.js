0b2b507013be24bae5132391357b54f419333c71
Alexis G <alexis.gontier03@gmail.com>
2024-04-07 10:57:08 +0200

    slider fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,41 @@ moon.addEventListener('click', () => {
     sun.style.display = 'block'
     moon.style.display = 'none'
 })
+
+// SLIDER
+let img_slider = document.querySelectorAll('.slider .img-slider img');
+let main_img = document.querySelector('.slider .main-img img');
+
+img_slider.forEach((picture) => {
+    picture.addEventListener('mouseover', () => {
+        let newSrc = picture.getAttribute('src');
+        main_img.setAttribute('src', newSrc);
+    });
+});
+
+// SLIDER RESPOSIVE
+let btn_right = document.getElementById('btn-right');
+let btn_left = document.getElementById('btn-left');
+let j = 1;
+
+btn_right.addEventListener('click', () => {
+    if (j >= 4) {
+        j = 1;
+        main_img.setAttribute('src', "./assets/img/slider-" + j + ".png");
+    } 
+    else {
+        j += 1;
+        main_img.setAttribute('src', "./assets/img/slider-" + j + ".png");
+    }
+});
+
+btn_left.addEventListener('click', () => {
+    if (j <= 1) {
+        j = 4;
+        main_img.setAttribute('src', "./assets/img/slider-" + j + ".png");
+    } 
+    else {
+        j -= 1;
+        main_img.setAttribute('src', "./assets/img/slider-" + j + ".png");
+    }
+});
